b23ae1dbbefddf92cd6a62412f664f277595c074
pixie-grasper <himajinn13sei@gmail.com>
2016-04-11 12:53:06 +0900

    grouped console-out functions and Implemented console_out.printi

---
+++ kernel/console-out.asm
@@ -0,0 +1,70 @@
+%ifndef CONSOLE_OUT_ASM_
+%define CONSOLE_OUT_ASM_
+
+%include "atomic.asm"
+
+console_out:
+  ; in: si = address of asciz string
+.prints:
+  mov rdi, .lock
+  call atomic.lock
+  mov ah, 0x07
+  mov rdi, [.current_pos]
+.prints.1:
+  mov al, [rsi]
+  test al, al
+  jz .prints.2
+  mov [rdi], ax
+  add rdi, 2
+  inc rsi
+  jmp .prints.1
+.prints.2:
+  mov [.current_pos], rdi
+  mov rdi, .lock
+  call atomic.unlock
+  ret
+
+  ; in: a = signed integer
+.printi:
+  xor rdx, rdx
+  xor ecx, ecx
+  push rcx  ; terminater
+  mov rdi, 10
+  test rax, rax
+  jns .printi.1
+  neg rax
+  neg ecx
+.printi.1:
+  test rax, rax
+  jz .printi.2
+  div rdi
+  add edx, 0x0730
+  push rdx
+  xor edx, edx
+  jmp .printi.1
+.printi.2:
+  mov rdi, .lock
+  call atomic.lock
+  mov rdi, [.current_pos]
+  test ecx, ecx
+  jns .printi.3
+  mov ax, 0x072d
+  mov [edi], ax
+  add edi, 2
+.printi.3:
+  pop rax
+  test eax, eax
+  jz .printi.4
+  mov [edi], ax
+  add edi, 2
+  jmp .printi.3
+.printi.4:
+  mov [.current_pos], rdi
+  mov rdi, .lock
+  call atomic.unlock
+  ret
+
+.current_pos: dq 0x000b8000
+.lock: dd 0
+
+%endif  ; CONSOLE_OUT_ASM_
--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -5,38 +5,16 @@
 entry:
   mov rsp, 0x00080000
   mov rsi, okmsg
-  call print
+  call console_out.prints
+  mov rax, 1234567890
+  call console_out.printi
   jmp end
 
 end:
   hlt
   jmp end
 
-  ; in: si = address of asciz string
-print:
-  mov rdi, graphics_lock
-  call atomic.lock
-  mov ah, 0x07
-  mov edi, [graphics_current_pos]
-.l1:
-  mov al, [rsi]
-  test al, al
-  jz .l2
-  mov [edi], ax
-  add edi, 2
-  inc rsi
-  jmp .l1
-.l2:
-  mov [graphics_current_pos], edi
-  mov rdi, graphics_lock
-  call atomic.unlock
-  ret
+%include "console-out.asm"
 
-%include "atomic.asm"
-
-  align 16
-  section .data vstart=0x2000+($-$$)
-graphics_current_pos: dd 0x000b8000
-graphics_lock: dd 0
 okmsg: db 'Hello.', 0
 
